1c3659fbdd2fcb7c84a5108571ef387c96ec4736
Laurie Waller <lauriewaller7@gmail.com>
2021-08-20 00:34:18 -0700

    create formVisibleOnPagse functionality in TapRoom

--- src/components/TapRoom.js
+++ src/components/TapRoom.js
@@ -8,6 +8,30 @@ class TapRoom extends React.Component {
       masterBeerList: []
     };
   }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !this.prevState.formVisibleOnPage
+    }));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewBeerForm />;
+    } else {
+      currentlyVisibleState = <BeerList />;
+    }
+
+    return (
+      <>
+      {currentlyVisibleState}
+      <button onClick={this.handleClick}>{buttonText}</button>
+      </>
+    );
+  }
 }
 
 export default TapRoom;
