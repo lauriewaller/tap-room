c01dfe6459488f5bda7872f7d2b3cfbbdcd56505
Laurie Waller <lauriewaller7@gmail.com>
2021-08-20 00:48:29 -0700

    create ReusableForm

---
+++ src/components/NewBeerForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
+
+function NewBeerForm(props) {
+  return(
+    <p></p>
+  )
+}
---
+++ src/components/ReusableForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props) {
+  return (
+    <>
+    <form onSubmit={props.formSubmissionHandler}>
+      <input 
+      type='text'
+      name='name'
+      placeholder='Name'/>
+      <input 
+      type='text'
+      name='brand'
+      placeholder='Brand'/>
+      <input 
+      type='text'
+      name='price'
+      placeholder='Price' />
+      <input 
+      type='text'
+      name='alcohol-content'
+      placeholder='Alcohol content'/>
+      <button type='submit'>{props.buttonText}</button>
+    </form>
+    </>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: propTypes.func,
+  buttonText: propTypes.string
+};
+
+export default ReusableForm;
