5b08f456e64b594ff473dc8049e03a63a93fbcf4
Laurie Waller <lauriewaller7@gmail.com>
2021-08-21 17:18:56 -0700

    move edit form file

---
+++ src/components/EditBeerForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from "prop-types";
+
+function EditBeerForm(props){
+  const { beer } = props;
+
+  function handleEditBeerFormSubmission(event) {
+    event.preventDefault();
+    props.onEditBeer({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: beer.id});
+  }
+
+  return (
+    <>
+      <ReusableForm 
+        formSubmissionHandler={handleEditBeerFormSubmission}
+        buttonText="Edit Beer" />
+    </>
+  );
+}
+
+EditBeerForm.propTypes = {
+  beer: PropTypes.object,
+  onEditBeer: PropTypes.func
+};
+
+export default EditBeerForm;
